8180c08839f8f7a90ce01933f70200a57e2f4b9c
Barta Ágnes <barta.agi27@gmail.com>
2014-11-22 09:30:39 +0100

    Adatmodell

---
+++ src/MVCWebExample/MVCWebExample/Controllers/UserController.cs
@@ -0,0 +1,47 @@
+﻿using MVCWebExample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MVCWebExample.Controllers
+{
+    public class UserController : ApiController
+    {
+        public IEnumerable<User> GetAllFacts()
+        {
+            return facts;
+        }
+
+        public User getUserByID(int id)
+        {
+            User fact = (from f in facts where f.ID == id select f).FirstOrDefault();
+            if (fact == null)
+                throw new HttpResponseException(this.Request.CreateResponse(HttpStatusCode.NotFound));
+
+            return fact;
+        }
+
+        public HttpResponseMessage PostFact(User user)
+        {
+            if (!this.ModelState.IsValid)
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.BadRequest));
+
+            facts.Add(user);
+            HttpResponseMessage response = this.Request.CreateResponse(HttpStatusCode.Created, user);
+            response.Headers.Location = new Uri(Url.Link("DefaultApi", new { id = user.ID }));
+
+            return response;
+        }
+
+        private static List<User> facts = new List<User>(){
+            new User{ID=1, Token="aa", isBanned=false},
+            new User{ID=2, Token="ab", isBanned=false},
+            new User{ID=3, Token="ccc", isBanned=true},
+        };
+    }
+
+    
+}
---
+++ src/MVCWebExample/MVCWebExample/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCWebExample.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public String Token { get; set; }
+        public Boolean isBanned { get; set; }
+    }
+}

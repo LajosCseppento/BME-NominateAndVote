e260a6e52e68c709ff312381dc698f183e3ef2d6
Barta Ágnes <barta.agi27@gmail.com>
2014-11-28 22:58:23 +0100

    PollController

---
+++ src/NominateAndVote/RestService/Controllers/PollController.cs
@@ -0,0 +1,48 @@
+﻿using NominateAndVote.DataModel;
+using NominateAndVote.DataModel.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RestService.Controllers
+{
+    public class PollController : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // GET api/<controller>/5
+        public Poll Get(int id)
+        {
+            DataModel d=new DataModel();
+            return d.Polls[id];
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}

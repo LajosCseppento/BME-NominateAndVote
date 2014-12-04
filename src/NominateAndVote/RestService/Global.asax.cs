9817eb608c1eb74e6b72868c0c051fc4e3ae1a90
Lajos Cseppentő <cseppento@outlook.com>
2014-12-04 14:33:50 +0100

    Added JSONP for RestService

--- src/NominateAndVote/RestService/Global.asax.cs
+++ src/NominateAndVote/RestService/Global.asax.cs
@@ -1,8 +1,11 @@
-﻿using System.Web;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using WebApiContrib.Formatting.Jsonp;
 
 namespace NominateAndVote.RestService
 {
@@ -15,6 +18,15 @@ namespace NominateAndVote.RestService
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            // formatter config:
+            var formatters = GlobalConfiguration.Configuration.Formatters;
+            var jsonFormatter = formatters.JsonFormatter;
+            jsonFormatter.SerializerSettings = new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() };
+
+            // add JSONP formatter
+            var jsonpFormatter = new JsonpMediaTypeFormatter(formatters.JsonFormatter);
+            formatters.Insert(0, jsonpFormatter);
         }
     }
 }
